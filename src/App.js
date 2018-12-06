8b024387a1fb4875c4cfd735d65cc73951f18aea
Donnie <donnie.ccj@gmail.com>
2018-12-06 13:48:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import ProgressBar from './components/ProgressBar';
+import ValidationPage from './components/ValidationPage';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <ProgressBar />
+        <ValidationPage />
       </div>
     );
   }
