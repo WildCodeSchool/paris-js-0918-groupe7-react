c2ac53ed386b24ffbbee7141107dfcebc572152a
Philippe Yu <yu.philippe@gmail.com>
2018-12-06 13:31:14 +0100

    chgt police THANK YOU

--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -3,14 +3,18 @@ import CardIntro from './CardIntro'
 import Grid from '@material-ui/core/Grid';
 import { Typography } from '@material-ui/core';
 import './IntroPage.css';
-import Img from '../images/intro_image.png';
+import ImgIntro from '../images/intro_image.png';
 
 
 class IntroPage extends Component {
     render(){
         return (
             <div>
-                <Grid container className='mabite'>
+                <Grid container className='gridintro' style={{
+                    backgroundColor: 'rgb(125,146, 177)',
+                    position: 'absolute',
+                    minHeight: '100%'
+                }}>
                 
                     <Grid item xs={12} sm={6}>
 
@@ -20,7 +24,7 @@ class IntroPage extends Component {
                         verticalAlign: 'middle',
                         color: 'white',
                         margin: '5% auto',
-                        }} variant="display3" gutterBottom> Thank you for creating you account !
+                        }} variant="subtitle1" gutterBottom> Thank you for creating you account !
                         </Typography>
 
                         <CardIntro />
@@ -28,7 +32,7 @@ class IntroPage extends Component {
                     </Grid>
                     
                     <Grid className='grid2' style={{
-                        backgroundImage: `url(${Img})`,
+                        backgroundImage: `url(${ImgIntro})`,
                         backgroundSize: 'auto',
                         backgroundRepeat: 'no-repeat',
                         
